36c155862a88200a1ef2685a6dcc3f4b6d5ec859
Koohii <156883939+koohiimc@users.noreply.github.com>
2024-06-14 18:59:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ app.get('/', async (req, res) => {
 
 app.listen(3000, function(err){
     if (err) console.log("Error in server setup")
-    console.log("Server listening on Port", PORT);
+    console.log("Server listening on Port 3000");
 })
